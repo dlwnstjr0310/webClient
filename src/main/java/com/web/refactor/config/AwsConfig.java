43f2d3790c4aaca0317ef0fd11975b18e032e7e6
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-12-29 17:47:27 +0900

    Update : Server
    # Server
    - jenkinsfile 수정
    - awsConfig 파일 추가

--- src/main/java/com/web/refactor/config/AwsConfig.java
+++ src/main/java/com/web/refactor/config/AwsConfig.java
@@ -1,21 +1,28 @@
 package com.web.refactor.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
 @Deprecated
-//@Configuration
+@Configuration
 public class AwsConfig {
 
-	//todo: 추후 aws 계정 만들고 다시 활성화 ( CLI 설정 해야함 )
+	@Value("${AWS_ACCESS_KEY_ID}")
+	private String accessKey;
 
-//	@Value("${AWS_ACCESS_KEY_ID}")
-//	private String accessKey;
-//
-//	@Value("${AWS_SECRET_ACCESS_KEY}")
-//	private String secretKey;
-//
-//	@Bean
-//	@Primary
-//	public AWSCredentialsProvider buildAWSCredentialsProvider() {
-//		AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-//		return new AWSStaticCredentialsProvider(awsCredentials);
-//	}
+	@Value("${AWS_SECRET_ACCESS_KEY}")
+	private String secretKey;
+
+	@Bean
+	@Primary
+	public AWSCredentialsProvider buildAWSCredentialsProvider() {
+		AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+		return new AWSStaticCredentialsProvider(awsCredentials);
+	}
 }
